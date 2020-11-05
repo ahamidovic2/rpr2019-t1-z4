fc559bc59c6b725a193bc1a773b1059bbfad265c
ahamidovic2 <ahamidovic2@etf.unsa.ba>
2020-11-05 17:56:57 +0100

    zad zvrs

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int brojArtikla = 0;
+
+    public boolean dodajArtikl(Artikl artikl) {
+        if (brojArtikla < 50) {
+            artikli[brojArtikla++] = new Artikl(artikl.getNaziv(), artikl.getCijena(), artikl.getKod());
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < brojArtikla; i++) {
+            if (artikli[i].getKod() == kod) {
+                Artikl noviArtikl = new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i] = null;
+                if (brojArtikla - i >= 0) System.arraycopy(artikli, i + 1, artikli, i, brojArtikla - i);
+                artikli[brojArtikla] = null;
+                brojArtikla--;
+
+                return noviArtikl;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int cijena = 0;
+        for (int i = 0; i < brojArtikla; i++) {
+            cijena += artikli[i].getCijena();
+        }
+        return cijena;
+    }
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int brojArtikla = 0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if (brojArtikla < 50) {
+            artikli[brojArtikla++] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < brojArtikla; i++) {
+            if (artikli[i].getKod() == kod) {
+                Artikl noviArtikl = new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i] = null;
+                if (brojArtikla - i >= 0) System.arraycopy(artikli, i + 1, artikli, i, brojArtikla - i);
+                artikli[brojArtikla] = null;
+                brojArtikla--;
+
+                return noviArtikl;
+            }
+        }
+        return null;
+    }
+
+
 }
